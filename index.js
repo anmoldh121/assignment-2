98b2cdf9f5ba16e317abf5c3710cd3cc9494ce9f
Beast <anmoldh121@gmail.com>
2021-04-01 18:16:17 +0530

    extra removed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 
 const app = express();
 const db = new sqlite3.Database(":memory", (err) => {
